45457064a2aff2c9de5140e40308a895eb86045b
phanhduchuy <phanhoangduchuyy@gmail.com>
2023-10-05 13:28:29 +0700

    build code base

--- web/src/main/java/com/pbl/loadtestweb/web/LoadTestWebApplication.java
+++ web/src/main/java/com/pbl/loadtestweb/web/LoadTestWebApplication.java
@@ -1,4 +1,4 @@
-package com.pbl.loadtestweb;
+package com.pbl.loadtestweb.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
