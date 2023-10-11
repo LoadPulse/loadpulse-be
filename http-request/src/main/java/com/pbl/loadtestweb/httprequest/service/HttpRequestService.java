c228fc9b7fd4473c11fdf10f5b46d11c73e3b4e1
phanhduchuy <phanhoangduchuyy@gmail.com>
2023-10-11 13:27:55 +0700

    add api endpoint for testing with method post

--- http-request/src/main/java/com/pbl/loadtestweb/httprequest/service/HttpRequestService.java
+++ http-request/src/main/java/com/pbl/loadtestweb/httprequest/service/HttpRequestService.java
@@ -1,7 +1,9 @@
 package com.pbl.loadtestweb.httprequest.service;
 
+import com.pbl.loadtestweb.httprequest.payload.request.HttpPostRequest;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 public interface HttpRequestService {
-  SseEmitter handleMethodGetLoadTestWeb(String url, int threadCount, int iterations);
+  SseEmitter handleLoadTestWeb(
+      String url, int threadCount, int iterations, String method, HttpPostRequest httpPostRequest);
 }
